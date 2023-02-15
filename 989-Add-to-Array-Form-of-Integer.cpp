62915e881c6fbbce04284ab74fdbd64ad8bbbc23
Akash Yadav <91262354+akash701752@users.noreply.github.com>
2023-02-15 23:01:08 +0530

    Create 989-Add-to-Array-Form-of-Integer.cpp

---
+++ 989-Add-to-Array-Form-of-Integer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        int n = num.size() ;
+        int carry = 0 ;
+        for(int i=n-1;i>=0;i--)
+        {
+            int val = k%10+ num[i] + carry ;
+            num[i] = val%10 ;
+            k = k/10 ;
+            carry = val/10 ;
+            if(k==0 && carry==0){
+                break ;
+            }
+        }
+        while(k){
+            int val = k%10 + carry ;
+            k = k/10 ;
+            num.insert(num.begin(),val%10) ;
+            carry = val/10 ;
+        }
+        if(carry){
+                num.insert(num.begin(),carry) ;
+            }
+        return num ;
+    }
+};
