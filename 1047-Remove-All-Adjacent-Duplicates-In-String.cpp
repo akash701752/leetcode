008639af3d8effa860c61c1ba523b365e7403207
Akash Yadav <91262354+akash701752@users.noreply.github.com>
2023-03-05 08:52:58 +0530

    Create 1047-Remove-All-Adjacent-Duplicates-In-String.cpp

---
+++ 1047-Remove-All-Adjacent-Duplicates-In-String.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string removeDuplicates(string s) 
+    {
+      string ans;
+      for(int i=0;i<s.size();i++){
+            if(ans.size()>0 &&ans.back() == s[i]){
+                ans.pop_back();
+            }
+            else{
+                ans+=s[i];
+            }
+        }
+        return ans;
+    }
+};
