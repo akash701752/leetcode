87bbe1aacfb9f96d1c9112e474bfd89486f7055b
Akash Yadav <91262354+akash701752@users.noreply.github.com>
2023-09-14 12:15:15 +0530

    Create  Code  Testcase Testcase33-Search in-Rotated-Sorted-Array.cpp

---
+++ Code  Testcase Testcase33-Search in-Rotated-Sorted-Array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    
+    int search(vector<int>& nums, int target) {
+        int n = nums.size() ;
+        int ans = -1 ;
+        int low = 0 , high = n-1;
+        while(low<=high){
+            int mid = low+ (high-low)/2 ;
+            if(nums[mid]==target){
+                ans = mid ;
+                break ;
+            }
+            if(nums[low]<= nums[mid]){ // left sorted
+                if(nums[low]<=target && target<=nums[mid]){
+                    high = mid-1;
+                }
+                else{
+                    low = mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<=target && target<=nums[high]){
+                    low = low+1;
+                }
+                else{
+                    high = mid-1;
+                }
+            }
+        }
+        return ans ;
+    }
+};
