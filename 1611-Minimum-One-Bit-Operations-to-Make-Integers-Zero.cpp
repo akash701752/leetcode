8589ddfd6cd55ee540331736f2327a32f49bba10
Akash Yadav <91262354+akash701752@users.noreply.github.com>
2023-11-30 15:15:01 +0530

    Create 1611-Minimum-One-Bit-Operations-to-Make-Integers-Zero.cpp

---
+++ 1611-Minimum-One-Bit-Operations-to-Make-Integers-Zero.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        int zero = 0, one = 0;
+	    for (int i = 1; n; n >>= 1, i <<= 1){
+		    one += i;
+		    if (n % 2){
+                swap(one, zero);
+            }
+        }
+	return zero;
+    }
+};
