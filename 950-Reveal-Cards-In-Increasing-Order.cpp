3401a7763b6ac6bd6a1af8097753c3ecb6942208
Akash Yadav <91262354+akash701752@users.noreply.github.com>
2024-04-10 19:54:06 +0530

    Create 950-Reveal-Cards-In-Increasing-Order.cpp

---
+++ 950-Reveal-Cards-In-Increasing-Order.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> deckRevealedIncreasing(vector<int>& deck) {
+        sort(deck.begin(), deck.end());
+        vector<int> res(deck.size(), 0);
+        int i=0;
+        bool skipped = true;
+		
+        while(i<deck.size()) {
+            for(int j=0;j<res.size();j++)
+                if(res[j]==0){
+                    if(skipped){
+                    res[j] = deck[i++];
+                    skipped = false;
+                    }
+                    else
+                        skipped = true;
+                }
+        }
+        return res;
+    
+    }
+};
