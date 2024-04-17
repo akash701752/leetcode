fa503339bbece19d6bd5711f290744564f808ef1
Akash Yadav <91262354+akash701752@users.noreply.github.com>
2024-04-17 13:32:49 +0530

    Create 988-Smallest-String-Starting-From-Leaf.cpp

---
+++ 988-Smallest-String-Starting-From-Leaf.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void solve(TreeNode* root,string s,vector<string> &v)
+    {
+        if(root==nullptr)
+            return ;
+        s+=(char)(root->val+97);
+        if(!root->left&&!root->right)
+        {
+            string temp=s;
+            reverse(temp.begin(),temp.end());
+            v.push_back(temp);
+        }
+        solve(root->left,s,v);
+        solve(root->right,s,v);
+    }
+    string smallestFromLeaf(TreeNode* root) {
+        vector<string> v;
+        string s="";
+        solve(root,s,v);
+        sort(v.begin(),v.end());
+        return v[0];
+        
+    }
+};
