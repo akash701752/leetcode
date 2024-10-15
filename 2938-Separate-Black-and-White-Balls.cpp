49868fdaf5432e72b51b372a60ba2cba070ce409
Akash Yadav <91262354+akash701752@users.noreply.github.com>
2024-10-15 19:34:09 +0530

    Create 2938-Separate-Black-and-White-Balls.cpp

---
+++ 2938-Separate-Black-and-White-Balls.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        int n = s.length();
+        long long ans = 0;
+        int cnt = 0; 
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '0') {
+                ans += cnt;
+            } 
+            else {
+                cnt++;
+            }
+        }
+        return ans;
+    }
+};
